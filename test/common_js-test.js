4421b13f7e3ae34869f158090ec7d66c6d9c1748
Ryan Rauh <rauh.ryan@gmail.com>
2015-03-17 11:10:39 -0500

    Adds a check for the commonjs export syntax and uses that to declare the name of es6 export
     - first basic test passing

---
+++ test/common_js-test.js
@@ -0,0 +1,23 @@
+var assert = require('chai').assert;
+var path = require('path');
+var fs = require('fs');
+var helper = require('./test_helper');
+var migrates = helper.migrates;
+var it = helper.it;
+var fs = require('fs');
+
+describe('migrating models', function(){
+  before(function(){
+    this.migrator = helper.migrator({inputFixtures: 'common_js'});
+    this.migrator.run();
+  });
+
+  after(function(){
+    this.migrator.clean();
+  });
+
+  describe('Works with application file', function(){
+    it(migrates('application.js'));
+  });
+
+});
---
+++ test/fixtures/common_js/input/application.js
@@ -0,0 +1,7 @@
+var App = Ember.Application.extend({
+  someProperty: function() {
+    console.log('hello App');
+  }
+});
+
+module.exports = App;
