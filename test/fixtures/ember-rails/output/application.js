a759516ff17f67308fbef453ca8844cca7dfc06d
Dave Laird <dlaird@godaddy.com>
2015-03-27 09:11:47 -0700

    Added a broken tests for #56

---
+++ test/fixtures/ember-rails/output/application.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require pos
+
+var App = Ember.Application.create({
+  LOG_ACTIVE_GENERATION: true,
+  LOG_MODULE_RESOLVER: true,
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS: true,
+});
+
+export default App;
